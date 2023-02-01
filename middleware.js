329ad0b486abc46039175225f83ce0c23a3221fc
Roshan Paudel <rozanpoudel@gmail.com>
2023-02-01 20:47:25 +0545

    formatted middleware.js

--- middleware.js
+++ middleware.js
@@ -1,8 +1,8 @@
-const Campground = require('./models/campground');
-const Review = require('./models/review');
+const Campground = require("./models/campground");
+const Review = require("./models/review");
 module.exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
-    req.flash('error', 'You must be logged in first');
+    req.flash("error", "You must be logged in first");
     const nextTo = req.originalUrl;
     return res.redirect(`/login?nextTo=${nextTo}`);
   }
@@ -12,7 +12,7 @@ module.exports.isAuthor = async (req, res, next) => {
   const { id } = req.params;
   const campground = await Campground.findById(id);
   if (!campground.author.equals(req.user._id)) {
-    req.flash('error', 'Opps You do not have permission to do that');
+    req.flash("error", "Opps You do not have permission to do that");
     return res.redirect(`/campgrounds/${id}`);
   }
   next();
@@ -22,7 +22,7 @@ module.exports.isReviewAuthor = async (req, res, next) => {
   const { id, reviewId } = req.params;
   const review = await Review.findById(reviewId);
   if (!review.author.equals(req.user._id)) {
-    req.flash('error', 'Opps You do not have permission to do that');
+    req.flash("error", "Opps You do not have permission to do that");
     return res.redirect(`/campgrounds/${id}`);
   }
   next();
