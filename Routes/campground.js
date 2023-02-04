5e35ad8e26b9759cb1c59d688fc86e16d8a2c360
Roshan Paudel <rozanpoudel@gmail.com>
2023-02-04 17:04:41 +0545

    image uploading feature added

--- Routes/campground.js
+++ Routes/campground.js
@@ -1,16 +1,21 @@
-const express = require('express');
+const express = require("express");
 router = express.Router();
-const catchAsync = require('../utils/CatchAsync');
-const { validateCampground } = require('../utils/validations');
-const Campground = require('../models/campground');
-const campground = require('../controllers/campgrounds');
-const { isLoggedIn } = require('../middleware');
+const catchAsync = require("../utils/CatchAsync");
+const { validateCampground } = require("../utils/validations");
+const Campground = require("../models/campground");
+const campground = require("../controllers/campgrounds");
+const { isLoggedIn } = require("../middleware");
+
+// Multer setup
+const { storage } = require("../cloudinary");
+const multer = require("multer");
+const upload = multer({ storage });
 
 const isAuthor = async (req, res, next) => {
   const { id } = req.params;
   const campground = await Campground.findById(id);
   if (!campground.author.equals(req.user._id)) {
-    req.flash('error', 'Opps You do not have permission to do that');
+    req.flash("error", "Opps You do not have permission to do that");
     res.redirect(`/campgrounds/${id}`);
   }
   next();
@@ -18,19 +23,21 @@ const isAuthor = async (req, res, next) => {
 
 //...................All Campgrounds..................
 router
-  .route('/')
+  .route("/")
   .get(catchAsync(campground.index))
   .post(
     isLoggedIn,
+    upload.array("image"),
     validateCampground,
     catchAsync(campground.createCampground)
   );
+
 //....................New Campground.....................
-router.get('/new', isLoggedIn, campground.renderNewForm);
+router.get("/new", isLoggedIn, campground.renderNewForm);
 
 //......................Show campground.................
 router
-  .route('/:id')
+  .route("/:id")
   .get(catchAsync(campground.showCampground))
   //...................Edit campground..............
   .put(
@@ -43,11 +50,10 @@ router
   .delete(isLoggedIn, isAuthor, catchAsync(campground.deleteCampground));
 
 router.get(
-  '/:id/edit',
+  "/:id/edit",
   isLoggedIn,
   isAuthor,
   catchAsync(campground.renderEditForm)
 );
-
 
 module.exports = router;
