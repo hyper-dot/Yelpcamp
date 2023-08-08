2f474ce94559f267da20f08cf5cf696b8374e582
Roshan Paudel <rozanpoudel@gmail.com>
2023-08-08 15:01:09 +0545

    deployed in deta space

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -1,17 +1,17 @@
-const cloudinary = require("cloudinary").v2;
-const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_KEY,
-  api_secret: process.env.CLOUDINARY_SECRET,
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
-    folder: "Yelpcamp",
-    allowedFormats: ["jpeg", "png", "jpg"],
+    folder: 'Yelpcamp',
+    allowedFormats: ['jpeg', 'png', 'jpg'],
   },
 });
 
