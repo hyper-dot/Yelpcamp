a49792df5b53b535d1112cee49ca654245be72c6
Roshan Paudel <rozanpoudel@gmail.com>
2023-08-10 12:57:27 +0545

    campground controller code refactor

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,13 +1,16 @@
-const Campground = require("../models/campground");
-const Review = require("../models/review");
+const Campground = require('../models/campground');
 
 module.exports.index = async (req, res) => {
   const campgrounds = await Campground.find({});
-  res.render("campgrounds/index", { campgrounds });
+  res.render('campgrounds/index', { campgrounds });
 };
+
+// Campground New form
 module.exports.renderNewForm = (req, res) => {
-  res.render("campgrounds/new");
+  res.render('campgrounds/new');
 };
+
+// Create campground
 module.exports.createCampground = async (req, res, next) => {
   const campground = new Campground(req.body.campground);
   campground.images = req.files.map((f) => ({
@@ -17,25 +20,29 @@ module.exports.createCampground = async (req, res, next) => {
   campground.author = req.user._id;
   await campground.save();
   console.log(campground);
-  req.flash("success", "Successfully made a campground !!!");
+  req.flash('success', 'Successfully made a campground !!!');
   res.redirect(`campgrounds/${campground._id}`);
 };
+
+// Show campground
 module.exports.showCampground = async (req, res) => {
   const { id } = req.params;
   const campground = await Campground.findById(id)
     .populate({
-      path: "reviews",
+      path: 'reviews',
       populate: {
-        path: "author",
+        path: 'author',
       },
     })
-    .populate("author");
+    .populate('author');
   if (!campground) {
-    req.flash("error", "Opps cannot find the campground!!");
-    return res.redirect("/campgrounds");
+    req.flash('error', 'Opps cannot find the campground!!');
+    return res.redirect('/campgrounds');
   }
-  res.render("campgrounds/show", { campground });
+  res.render('campgrounds/show', { campground });
 };
+
+// Edit campground
 module.exports.editCampground = async (req, res) => {
   const { id } = req.params;
   const imgs = req.files.map((f) => ({ url: f.path, filename: f.filename }));
@@ -44,21 +51,24 @@ module.exports.editCampground = async (req, res) => {
     ...req.body.campground,
   });
   await campground.save();
-  req.flash("success", "Successfully Updated campground !!!");
+  req.flash('success', 'Successfully Updated campground !!!');
   res.redirect(`/campgrounds/${camp._id}`);
 };
 
+// Render new edit from
 module.exports.renderEditForm = async (req, res) => {
   const campground = await Campground.findById(req.params.id);
   if (!campground) {
-    req.flash("error", "Opps cannot find the campground!!");
-    return res.redirect("/campgrounds");
+    req.flash('error', 'Opps cannot find the campground!!');
+    return res.redirect('/campgrounds');
   }
-  res.render("campgrounds/edit", { campground });
+  res.render('campgrounds/edit', { campground });
 };
+
+// Delete campground
 module.exports.deleteCampground = async (req, res) => {
   const { id } = req.params;
   await Campground.findByIdAndDelete(id);
-  req.flash("success", "Successfully deleted the campground !!");
-  res.redirect("/campgrounds");
+  req.flash('success', 'Successfully deleted the campground !!');
+  res.redirect('/campgrounds');
 };
